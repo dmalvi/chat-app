dfe8bd82d9dd5b6d95fc449588c3d9c021d44bac
dmalvi <dmalvi@gmail.com>
2018-09-19 13:29:27 +0200

    keys and fb

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 //Inside of firebase.js file
 import firebase from 'firebase';
+require('dotenv').config();
 
 // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyBBpuLuv256zntQXoZA_9ONno1qHGVuEI8",
+    apiKey: process.env.REACT_APP_APIKEY,
     authDomain: "dm-project-8db52.firebaseapp.com",
     databaseURL: "https://dm-project-8db52.firebaseio.com",
     projectId: "dm-project-8db52",
     storageBucket: "dm-project-8db52.appspot.com",
-    messagingSenderId: "537523031186"
+    messagingSenderId: process.env.REACT_APP_MSENDERID
   };
 
 firebase.initializeApp(config);
