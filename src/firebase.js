f9ebdc36926ac7f876ad9aacb3f415f17c6686e1
dmalvi <dmalvi@gmail.com>
2018-09-19 11:57:36 +0200

    db settings

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import firebase from 'firebase';
 
 // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyBBpuLuv256zntQXoZA_9ONno1qHGVuEI8",
-    authDomain: "dm-project-8db52.firebaseapp.com",
-    databaseURL: "https://dm-project-8db52.firebaseio.com",
-    projectId: "dm-project-8db52",
-    storageBucket: "dm-project-8db52.appspot.com",
-    messagingSenderId: "537523031186"
+
   };
 
 firebase.initializeApp(config);
